c5c52da5a56a0be693b6a9f3e8ebdf61b972a27c
verenach <verenach@student.21-school.ru>
2023-08-25 20:45:03 +0700

    добавила методы, структуру для 5 и 6 частей

--- src/graph_classes/s21_graph_algorithms.h
+++ src/graph_classes/s21_graph_algorithms.h
@@ -4,6 +4,12 @@
 #include "s21_graph.h"
 
 namespace SimpleNavigator {
+struct TsmResult {
+  int* vertices;  // массив с искомым маршрутом (с порядком обхода вершин).
+                  // Вместо int* можно использовать std::vector<int>
+  double distance;  // длина этого маршрута
+};
+
 class GraphAlgorithms {
  public:
   using AdjacencyMatrix = std::vector<std::vector<int>>;
@@ -12,8 +18,14 @@ class GraphAlgorithms {
   GraphAlgorithms(const Graph& graph) : graph_(graph){};
   ~GraphAlgorithms() = default;
 
-  std::vector<int> DepthFirstSearch(Graph& graph, int startVertex);
-  std::vector<int> BreadthFirstSearch(Graph& graph, int startVertex);
+  std::vector<int> DepthFirstSearch(const Graph& graph, int startVertex) const;
+  std::vector<int> BreadthFirstSearch(const Graph& graph,
+                                      int startVertex) const;
+  int GetShortestPathBetweenVertices(const Graph& graph, int vertex1,
+                                     int vertex2) const;
+  AdjacencyMatrix GetShortestPathsBetweenAllVertices(Graph& graph) const;
+  AdjacencyMatrix GetLeastSpanningTree(const Graph& graph) const;
+  TsmResult SolveTravelingSalesmanProblem(const Graph& graph) const;
 
  private:
   Graph graph_;
