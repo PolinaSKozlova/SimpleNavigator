296897d793411b3b083a24c3ad33a67f5fbc3d3d
verenach <verenach@student.21-school.ru>
2023-08-25 17:39:57 +0700

    добавила метод для загрузки файла, вспомогательные методы

--- src/graph_classes/s21_graph.cc
+++ src/graph_classes/s21_graph.cc
@@ -1,13 +1,33 @@
 #include "s21_graph.h"
 
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
 namespace SimpleNavigator {
 Graph::Graph(size_t size) : size_(size) { AllocateMemoryForGraphMatrix(); }
 
 Graph::~Graph() { FreeMemoryForGraphMatrix(); }
 
-void Graph::LoadGraphFromFile(std::string filename) {}
+void Graph::LoadGraphFromFile(std::string filename) {
+  std::filesystem::path absolute_path = std::filesystem::absolute(filename);
+  std::ifstream fin;
+  std::string line;
+  fin.open(absolute_path.string());
+  if (!fin.is_open()) throw std::invalid_argument("Can't open file");
+  getline(fin, line);
+  GetSize(line);
+  int i = 0;
+  while (!fin.eof()) {
+    getline(fin, line);
+    if (line.empty()) break;
+    ReadElementsFromAdjacencyMatrix(line, graph_matrix_[i++]);
+  }
+  fin.close();
+}
 
-void Graph::ExportGraphToDot(std::string filename) {}
+// void Graph::ExportGraphToDot(std::string filename) {}
 
 void Graph::AllocateMemoryForGraphMatrix() {
   graph_matrix_.resize(size_);
@@ -22,6 +42,28 @@ void Graph::FreeMemoryForGraphMatrix() {
   }
   graph_matrix_.clear();
   size_ = 0;
+}
+
+void Graph::GetSize(const std::string& size) {
+  FreeMemoryForGraphMatrix();
+  size_ = std::stoi(size);
+  AllocateMemoryForGraphMatrix();
+}
+
+void Graph::ReadElementsFromAdjacencyMatrix(const std::string& line,
+                                            std::vector<int>& rows) {
+  std::istringstream iss(line);
+  std::string matrix_line{};
+  size_t pos{0};
+  while (iss >> matrix_line) {
+    std::istringstream sub_iss(matrix_line);
+    std::string sub_token;
+    getline(sub_iss, sub_token, ' ');
+    if (!sub_token.empty()) {
+      rows[pos++] = std::stoi(sub_token);
+    }
+  }
+  if (pos != size_) throw std::invalid_argument("Incorrect matrix size");
 }
 
 };  // namespace SimpleNavigator
--- src/graph_classes/s21_graph.h
+++ src/graph_classes/s21_graph.h
@@ -1,6 +1,7 @@
 #ifndef SIMPLE_NAVIGATOR_GRAPH_H
 #define SIMPLE_NAVIGATOR_GRAPH_H
 
+#include <iostream>
 #include <string>
 #include <vector>
 
@@ -14,9 +15,21 @@ class Graph {
   void LoadGraphFromFile(std::string filename);
   void ExportGraphToDot(std::string filename);
 
+  void print_graph() {
+    for (size_t i = 0; i < size_; ++i) {
+      for (size_t j = 0; j < size_; ++j) {
+        std::cout << graph_matrix_[i][j] << " ";
+      }
+      std::cout << std::endl;
+    }
+  }
+
  private:
   void AllocateMemoryForGraphMatrix();
   void FreeMemoryForGraphMatrix();
+  void GetSize(const std::string& size);
+  void ReadElementsFromAdjacencyMatrix(const std::string& line,
+                                       std::vector<int>& rows);
   size_t size_{};
   std::vector<std::vector<int>> graph_matrix_;
 };
