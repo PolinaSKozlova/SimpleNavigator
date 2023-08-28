71e6fa495c56ca758f51984012b36ccab313ac79
verenach <verenach@student.21-school.ru>
2023-08-28 18:44:31 +0700

    добивила класс для консольного интерфейса

---
+++ src/console/console.h
@@ -0,0 +1,14 @@
+#ifndef SIMPLE_NAVIGATOR_CONSOLE_H
+#define SIMPLE_NAVIGATOR_CONSOLE_H
+
+namespace SimpleNavigator {
+class ConsoleApp {
+ public:
+  ConsoleApp() = default;
+  ~ConsoleApp() = default;
+
+ private:
+};
+};  // namespace SimpleNavigator
+
+#endif  // SIMPLE_NAVIGATOR_CONSOLE_H
