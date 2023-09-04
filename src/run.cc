cd6e0b22fdb9f47060f9375f906be119eefe46d2
verenach <verenach@student.21-school.ru>
2023-09-04 12:49:24 +0700

    проверка консоли

--- src/run.cc
+++ src/run.cc
@@ -1,13 +1,14 @@
+#include "console/console.h"
 #include "graph_classes/s21_graph.h"
 #include "graph_classes/s21_graph_algorithms.h"
 
 int main() {
-  SimpleNavigator::Graph graph;
-  graph.LoadGraphFromFile("graph_examples/graph_6");
-  graph.print_graph();
+  // SimpleNavigator::Graph graph;
+  // graph.LoadGraphFromFile("graph_examples/graph_6");
+  // graph.print_graph();
   // graph.ExportGraphToDot("graph.dot");
 
-  SimpleNavigator::GraphAlgorithms graph_algorithms;
+  // SimpleNavigator::GraphAlgorithms graph_algorithms;
   // graph_algorithms.DepthFirstSearch(graph, 1);
   // graph_algorithms.PrintVector(graph_algorithms.DepthFirstSearch(graph, 1));
   // graph_algorithms.BreadthFirstSearch(graph, 1);
@@ -21,10 +22,13 @@ int main() {
   // std::cout << " shortest_paths after FW" << std::endl;
   // graph_algorithms.PrintMatrix(graph_matrix);
 
-  SimpleNavigator::GraphAlgorithms::AdjacencyMatrix graph_matrix =
-      graph_algorithms.GetLeastSpanningTree(graph);
-  std::cout << " spanning tree " << std::endl;
-  graph_algorithms.PrintMatrix(graph_matrix);
-
+  // SimpleNavigator::GraphAlgorithms::AdjacencyMatrix graph_matrix =
+  //     graph_algorithms.GetLeastSpanningTree(graph);
+  // std::cout << " spanning tree " << std::endl;
+  // graph_algorithms.PrintMatrix(graph_matrix);
+  SimpleNavigator::ConsoleApp console_app;
+  console_app.ShowMenu();
+  // Menu menu;
+  // menu.ShowMenu();
   return 0;
 }
