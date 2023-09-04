976765e330bbe98513887b066771a69e83b0d683
verenach <verenach@student.21-school.ru>
2023-09-04 12:48:59 +0700

    удалила предыдущий файл с консолью, добавила const на методы Options

--- src/console/console.h
+++ src/console/console.h
@@ -6,7 +6,6 @@
 #include <iostream>
 #include <map>
 #include <string>
-#include <variant>
 
 #include "../graph_classes/s21_graph.h"
 #include "../graph_classes/s21_graph_algorithms.h"
@@ -17,18 +16,9 @@ class ConsoleApp {
   ConsoleApp() = default;
   ~ConsoleApp() = default;
   void ShowMenu() {
-    char menu_number{};
-    Graph graph;
-    GraphAlgorithms graph_algo;
-    while (menu_number != 'q') {
-      std::cout << "\nMenu:\n";
-      std::cout << "1. Download graph from file\n";
-      std::cout << "2. Breadth First Search\n";
-      std::cout << "3. Depth First Search\n";
-      std::cout << "4. Get Shortest Path Between Two Vertices\n";
-      std::cout << "5. Get Shortest Path Between All Vertices\n";
-      std::cout << "6. Get Least Spanning Tree\n";
-      std::cout << "7. Solve Traveling Salesman Problem\n";
+    int menu_number{};
+    while (menu_number != 8) {
+      DisplayMenu();
       std::cin >> menu_number;
       DoAlgorithms(menu_number);
     }
@@ -37,69 +27,80 @@ class ConsoleApp {
  private:
   void DoAlgorithms(const int menu_number) {
     std::system("clear");
-    int start_vertex{}, end_vertex{};
-    switch (menu_number) {
-      case '1': {
-        std::string file_name{};
-        std::cin >> file_name;
-        try {
-          graph_.LoadGraphFromFile(file_name);
-          std::cout << "Graph loaded\n";
-        } catch (std::invalid_argument& e) {
-          std::cout << e.what() << std::endl;
-        }
-        break;
+    std::map<int, std::function<void()>> menu_options;
+    menu_options[1] = std::bind(&ConsoleApp::Option_1, this);
+    menu_options[2] = std::bind(&ConsoleApp::Option_2, this);
+    menu_options[3] = std::bind(&ConsoleApp::Option_3, this);
+    menu_options[4] = std::bind(&ConsoleApp::Option_4, this);
+    menu_options[5] = std::bind(&ConsoleApp::Option_5, this);
+    menu_options[6] = std::bind(&ConsoleApp::Option_6, this);
+
+    try {
+      auto it = menu_options.find(menu_number);
+      if (menu_number == 8) {
+        std::cout << "Bye\n";
+      } else if (it != menu_options.end()) {
+        it->second();
+      } else {
+        std::cout << "Invalid option\n";
       }
-      case '2':
-        if (CheckGraph(start_vertex))
-          graph_algo_.PrintVector(
-              graph_algo_.BreadthFirstSearch(graph_, start_vertex));
-        break;
-      case '3':
-        if (CheckGraph(start_vertex))
-          graph_algo_.PrintVector(
-              graph_algo_.DepthFirstSearch(graph_, start_vertex));
-        break;
-      case '4':
-        if (CheckGraph()) {
-          std::cout << "Enter two verticies for getting shortest path: \n";
-          std::cin >> start_vertex >> end_vertex;
-          std::cout << graph_algo_.GetShortestPathBetweenVertices(
-                           graph_, start_vertex, end_vertex)
-                    << "\n";
-        }
-        break;
-      case '5':
-        if (CheckGraph())
-          graph_algo_.PrintMatrix(
-              graph_algo_.GetShortestPathsBetweenAllVertices(graph_));
-        break;
-      case '6':
-        if (CheckGraph())
-          graph_algo_.PrintMatrix(graph_algo_.GetLeastSpanningTree(graph_));
-        break;
-      case '7':
-        break;
+    } catch (std::exception& e) {
+      std::cout << e.what() << std::endl;
     }
   }
 
-  bool CheckGraph() {
-    if (graph_.IsEmpty()) {
-      std::cout << "You should choose the graph\n";
-      return false;
-    }
-    return true;
+  void DisplayMenu() {
+    std::cout << "\nMenu:\n";
+    std::cout << "1. Download graph from file\n";
+    std::cout << "2. Breadth First Search\n";
+    std::cout << "3. Depth First Search\n";
+    std::cout << "4. Get Shortest Path Between Two Vertices\n";
+    std::cout << "5. Get Shortest Path Between All Vertices\n";
+    std::cout << "6. Get Least Spanning Tree\n";
+    std::cout << "7. Solve Traveling Salesman Problem\n";
+    std::cout << "8. Exit\n";
   }
 
-  bool CheckGraph(int& vertex) {
-    if (graph_.IsEmpty()) {
-      std::cout << "You should choose the graph\n";
-      return false;
-    } else {
-      std::cout << "Enter the start vertex: \n";
-      std::cin >> vertex;
-      return true;
+  void Option_1() {
+    std::string file_name{};
+    std::cin >> file_name;
+    try {
+      graph_.LoadGraphFromFile(file_name);
+      std::cout << "Graph loaded\n";
+    } catch (std::invalid_argument& e) {
+      std::cout << e.what() << std::endl;
     }
+  }
+
+  void Option_2() const {
+    int start_vertex{};
+    std::cin >> start_vertex;
+    graph_algo_.PrintVector(
+        graph_algo_.BreadthFirstSearch(graph_, start_vertex));
+  }
+
+  void Option_3() const {
+    int start_vertex{};
+    std::cin >> start_vertex;
+    graph_algo_.PrintVector(graph_algo_.DepthFirstSearch(graph_, start_vertex));
+  }
+
+  void Option_4() const {
+    int start_vertex{};
+    int end_vertex{};
+    std::cout << "Enter the start and the end vertices: \n";
+    std::cin >> start_vertex >> end_vertex;
+    std::cout << graph_algo_.GetShortestPathBetweenVertices(
+        graph_, start_vertex, end_vertex);
+  }
+
+  void Option_5() const {
+    graph_algo_.PrintMatrix(
+        graph_algo_.GetShortestPathsBetweenAllVertices(graph_));
+  }
+
+  void Option_6() const {
+    graph_algo_.PrintMatrix(graph_algo_.GetLeastSpanningTree(graph_));
   }
 
   Graph graph_;
