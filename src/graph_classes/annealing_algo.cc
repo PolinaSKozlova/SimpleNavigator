4d003e8f8c28658eee2668ece8f7036936c7a37d
verenach <verenach@student.21-school.ru>
2023-09-07 17:03:19 +0700

    вынесла алгоритм иммитации отжига в отдельный класс

---
+++ src/graph_classes/annealing_algo.cc
@@ -0,0 +1,94 @@
+#include "annealing_algo.h"
+
+namespace SimpleNavigator {
+TsmResult AnnealingAlgorithms::SolveSailsmanProblem(const Graph& graph) const {
+  if (graph.IsEmpty()) throw std::invalid_argument("You should load graph!");
+
+  TsmResult result;
+
+  std::vector<int> current_solution = GenerateRandomSolution(graph);
+  double current_cost = CalculateCost(current_solution, graph);
+
+  result.vertices = current_solution;
+  result.distance = current_cost;
+
+  double temperature = kInitialTemperature;
+
+  while (temperature > kMinTemperature) {
+    for (int i = 0; i < kNumIterations; i++) {
+      std::vector<int> new_solution = GenerateNextSolution(current_solution);
+      double new_cost = CalculateCost(new_solution, graph);
+
+      double delta = new_cost - current_cost;
+
+      if (delta < 0 && std::exp(-delta / temperature) > RandomNumber()) {
+        current_solution = new_solution;
+        current_cost = new_cost;
+      }
+
+      //   double delta = current_cost - new_cost;
+
+      //   if (std::exp(delta / temperature) > RandomNumber()) {
+      //     current_solution = new_solution;
+      //     current_cost = new_cost;
+      //   }
+
+      if (current_cost < result.distance) {
+        result.vertices = current_solution;
+        result.distance = current_cost;
+      }
+
+      temperature *= kCoolingRate;
+    }
+  }
+
+  return result;
+}
+
+double AnnealingAlgorithms::RandomNumber() const noexcept {
+  std::default_random_engine generator(std::random_device{}());
+  std::uniform_real_distribution distribution(0.0, 1.0);
+  return distribution(generator);
+}
+
+int AnnealingAlgorithms::RandomVertex(int max_value) const noexcept {
+  std::default_random_engine generator(std::random_device{}());
+  std::uniform_int_distribution distribution(0, max_value);
+  return distribution(generator);
+}
+
+std::vector<int> AnnealingAlgorithms::GenerateRandomSolution(
+    const Graph& graph) const {
+  std::vector<int> start_path;
+  size_t vertex{};
+  int choice = RandomVertex(graph.GetSize() - 1);
+  while (vertex < graph.GetSize()) {
+    start_path.push_back(choice);
+    ++vertex;
+    choice = (choice + 1) % graph.GetSize();
+  }
+  //   start_path.push_back(start_path.front());
+  return start_path;
+}
+
+std::vector<int> AnnealingAlgorithms::GenerateNextSolution(
+    std::vector<int>& path) const {
+  int vertex1 = RandomVertex(path.size() - 1);
+  int vertex2 = RandomVertex(path.size() - 1);
+  std::swap(path[vertex1], path[vertex2]);
+  return path;
+}
+
+double AnnealingAlgorithms::CalculateCost(std::vector<int>& current_solution,
+                                          const Graph& graph) const {
+  double distance{};
+  for (size_t k = 0; k < current_solution.size() - 1; ++k) {
+    distance +=
+        graph.GetGraphMatrix()[current_solution[k]][current_solution[k + 1]];
+  }
+  distance += graph.GetGraphMatrix()[current_solution[graph.GetSize() - 1]]
+                                    [current_solution[0]];
+  return distance;
+}
+
+};  // namespace SimpleNavigator
---
+++ src/graph_classes/annealing_algo.h
@@ -0,0 +1,32 @@
+#ifndef SIMPLE_NAVIGATOR_ANNEALING_ALGORITHMS_H
+#define SIMPLE_NAVIGATOR_ANNEALING_ALGORITHMS_H
+
+#include <random>
+#include <vector>
+
+#include "s21_graph.h"
+
+namespace SimpleNavigator {
+class AnnealingAlgorithms {
+  const double kInitialTemperature = 1000.0;
+  const double kCoolingRate = 0.99;
+  const double kMinTemperature = 0.01;
+  const int kNumIterations = 1000;
+
+ public:
+  AnnealingAlgorithms() = default;
+  ~AnnealingAlgorithms() = default;
+  TsmResult SolveSailsmanProblem(const Graph& graph) const;
+
+ private:
+  double RandomNumber() const noexcept;
+  int RandomVertex(int max_value) const noexcept;
+  std::vector<int> GenerateRandomSolution(const Graph& graph) const;
+  std::vector<int> GenerateNextSolution(std::vector<int>& path) const;
+  double CalculateCost(std::vector<int>& current_solution,
+                       const Graph& graph) const;
+};
+
+};  // namespace SimpleNavigator
+
+#endif  // SIMPLE_NAVIGATOR_ANNEALING_ALGORITHMS_H
