76c76a0ce8d97aa3c4288f912e5db55b75c77405
KristinaSaraeva <kristinasaraeva660@gmail.com>
2023-09-08 15:00:45 +0700

    ant tests added

--- src/graph_tests/graph_salesman_problem_tests.cc
+++ src/graph_tests/graph_salesman_problem_tests.cc
@@ -7,11 +7,42 @@
 #include "../graph_classes/s21_graph.h"
 #include "../graph_classes/s21_graph_algorithms.h"
 
-TEST(ant_algorithm, test_1) {}
+TEST(ant_algorithm, test_1) {
+  SimpleNavigator::Graph graph;
+  graph.LoadGraphFromFile("graph_examples/graph_4");
+  SimpleNavigator::AntAlgorithm best_path;
+  best_path.RunAntAlgoritm(graph);
+  EXPECT_EQ(graph.GetSize() + 1, best_path.GetSolution().vertices.size());
+}
 
-TEST(ant_algorithm, test_2) {}
+TEST(ant_algorithm, test_2) {
+  SimpleNavigator::Graph graph;
+  graph.LoadGraphFromFile("graph_examples/graph_5");
+  SimpleNavigator::AntAlgorithm best_path;
+  best_path.RunAntAlgoritm(graph);
+  EXPECT_EQ(graph.GetSize() + 1, best_path.GetSolution().vertices.size());
+}
 
-TEST(ant_algorithm, test_3) {}
+TEST(ant_algorithm, test_3) {
+  SimpleNavigator::Graph graph;
+  graph.LoadGraphFromFile("graph_examples/graph_6_or");
+  SimpleNavigator::AntAlgorithm best_path;
+  try {
+    best_path.RunAntAlgoritm(graph);
+  } catch (std::invalid_argument &e) {
+    EXPECT_EQ("Sorry! There is no path!", std::string(e.what()));
+  }
+}
+
+TEST(ant_algorithm, test_4) {
+  SimpleNavigator::Graph graph;
+  SimpleNavigator::AntAlgorithm best_path;
+  try {
+    best_path.RunAntAlgoritm(graph);
+  } catch (std::invalid_argument &e) {
+    EXPECT_EQ("Sorry! There is no graph!", std::string(e.what()));
+  }
+}
 
 TEST(annealing_algorithm, test_1) {
   SimpleNavigator::Graph graph;
