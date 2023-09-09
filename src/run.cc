2589562f783d8c2bec025da13967b3aad24f32e8
KristinaSaraeva <kristinasaraeva660@gmail.com>
2023-09-09 16:17:29 +0700

    run again

---
+++ src/run.cc
@@ -0,0 +1,33 @@
+#include "graph_classes/s21_graph.h"
+#include "graph_classes/s21_graph_algorithms.h"
+
+int main() {
+  SimpleNavigator::Graph graph;
+  graph.LoadGraphFromFile("graph_examples/graph_4");
+  // graph.print_graph();
+  // graph.ExportGraphToDot("graph.dot");
+
+  SimpleNavigator::GraphAlgorithms graph_algorithms;
+  // graph_algorithms.DepthFirstSearch(graph, 1);
+  // graph_algorithms.PrintVector(graph_algorithms.DepthFirstSearch(graph, 1));
+  // graph_algorithms.BreadthFirstSearch(graph, 1);
+  // graph_algorithms.PrintVector(graph_algorithms.BreadthFirstSearch(graph,
+  // 1)); std::cout << "shortest way "
+  //            << graph_algorithms.GetShortestPathBetweenVertices(graph, 1, 7)
+  //            << std::endl;
+
+  // SimpleNavigator::GraphAlgorithms::AdjacencyMatrix graph_matrix =
+  //     graph_algorithms.GetShortestPathsBetweenAllVertices(graph);
+  // std::cout << " shortest_paths after FW" << std::endl;
+  // graph_algorithms.PrintMatrix(graph_matrix);
+
+  // SimpleNavigator::GraphAlgorithms::AdjacencyMatrix graph_matrix =
+  //     graph_algorithms.GetLeastSpanningTree(graph);
+  // std::cout << " spanning tree " << std::endl;
+  // graph_algorithms.PrintMatrix(graph_matrix);
+  SimpleNavigator::TsmResult res =
+      graph_algorithms.SolveTravelingSalesmanProblem(graph);
+  res.PrintTsmResult();
+
+  return 0;
+}
